def7f1a31cc44b39f0a774fd4c929f30960e9993
William Méndez <wmendezc19@gmail.com>
2016-10-22 22:56:25 -0500

    fixed bug in my webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ module.exports = {
    output : {
      path : "./priv/static/js",
      filename: "app.js"
-   }
+   },
 
    module: {
      loaders: [{
@@ -11,9 +11,8 @@ module.exports = {
         exclude: /node_modules/,
         loader: "babel",
         query: {
-           presets: {"es2015"]
+           presets: ["es2015"]
         }
      }]
    }
-
 }
